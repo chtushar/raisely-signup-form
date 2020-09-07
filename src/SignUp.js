97b7ca1461a7682bd53a097292efd0ded8104b72
Tushar Choudhari <choudharit23@gmail.com>
2020-09-08 04:36:08 +0530

    working with API

--- src/SignUp.js
+++ src/SignUp.js
@@ -2,27 +2,34 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function SignUp() {
-  const [signUpdata, setSignUpData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-  });
+  const [signedUp, setSignedUp] = useState(false);
   const [validated, setValidated] = useState(false);
   const [validationMessage, setValidationMessage] = useState('');
+  const [signedUpMessage, setSignedUpMessage] = useState('');
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     let formData = new FormData(e.target);
-    let newSignUpData = {};
+    let signUpData = {};
 
     for (let k of formData.entries()) {
-      newSignUpData[k[0]] = k[1];
+      signUpData[k[0]] = k[1];
     }
 
-    console.log(newSignUpData);
-    setSignUpData(newSignUpData);
+    await axios
+      .post('https://api.raisely.com/v3/signup', {
+        campaignUuid: '46aa3270-d2ee-11ea-a9f0-e9a68ccff42a',
+        data: signUpData,
+      })
+      .then(({ data }) => {
+        setUserName(data.data.preferredname);
+        setSignedUpMessage(data.message);
+        setSignedUp(true);
+      })
+      .catch((err) => {
+        setSignedUpMessage('Something went wrong. Try again.');
+      });
   }
 
   function handleStyling(target, c) {
@@ -54,7 +61,7 @@ function SignUp() {
               setValidationMessage('OK 🐣');
               setValidated(true);
             } else if (data.data.status === 'EXISTS') {
-              setValidationMessage('Try a new email ID.');
+              setValidationMessage('🚫 Try a new email ID.');
               handleStyling('red');
               setValidated(false);
             }
@@ -69,56 +76,70 @@ function SignUp() {
 
   return (
     <div className="signup-form-wrapper">
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <label id="firstName">
-          First Name:{' '}
-          <input
-            type="text"
-            name="firstName"
-            placeholder="First Name"
-            required
-          />
-        </label>
+      {signedUp ? (
+        <div>
+          <h2>Hey!</h2>
+          <p>{signedUpMessage}</p>
+        </div>
+      ) : (
+        <>
+          <h2>Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            <label id="firstName">
+              First Name:{' '}
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                required
+              />
+            </label>
 
-        <label id="lastName">
-          Last Name:{' '}
-          <input type="text" name="lastName" required placeholder="Last Name" />
-        </label>
+            <label id="lastName">
+              Last Name:{' '}
+              <input
+                type="text"
+                name="lastName"
+                required
+                placeholder="Last Name"
+              />
+            </label>
 
-        <label id="email">
-          <div>
-            Email:{' '}
-            <span className={!validated ? 'error-text' : 'success-text'}>
-              {validationMessage}
-            </span>
-          </div>
-          <input
-            type="email"
-            name="email"
-            required
-            onChange={handleEmailChange}
-            placeholder="Email"
-          />
-        </label>
+            <label id="email">
+              <div>
+                Email:{' '}
+                <span className={!validated ? 'error-text' : 'success-text'}>
+                  {validationMessage}
+                </span>
+              </div>
+              <input
+                type="email"
+                name="email"
+                required
+                onChange={handleEmailChange}
+                placeholder="Email"
+              />
+            </label>
 
-        <label id="password">
-          Password:{' '}
-          <input
-            type="password"
-            name="password"
-            required
-            placeholder="Password"
-          />
-        </label>
+            <label id="password">
+              Password:{' '}
+              <input
+                type="password"
+                name="password"
+                required
+                placeholder="Password"
+              />
+            </label>
 
-        <input
-          type="submit"
-          value="Sign Up"
-          disabled={!validated}
-          id="submit-signup"
-        />
-      </form>
+            <input
+              type="submit"
+              value="Sign Up"
+              disabled={!validated}
+              id="submit-signup"
+            />
+          </form>
+        </>
+      )}
     </div>
   );
 }
